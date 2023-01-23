a17536a4fa4db41e9738e07b903397c2cf52b1ec
justinloo3790 <justinloo85@gmail.com>
2023-01-23 21:39:08 +0800

    hi

--- api.py
+++ api.py
@@ -1,7 +1,4 @@
 import requests
 import json
-<<<<<<< HEAD
-lphmnlpkfgjhlfgjl;fgj,n.g;fvj,;fgj,gfv;lj,fgx;lj,gfl;pjngf;ljmgh;'jm'knjinioj
-=======
 
->>>>>>> 3fdd1d89bac8c4c6666efe72f2a08b92cb83128a
+
