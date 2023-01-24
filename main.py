9bd84b251988454c8eb4e6b3e33748b1914f0ae5
yanlingng <yanling0167@gmail.com>
2023-01-24 14:00:14 +0800

    commit code

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ with open("summary_report.txt", "w") as f:
     f.write("[HIGHEST OVERHEADS] {} EXPENSE: {}%\n".format(expense,expense_percentage))
     
     if cash_results == 0:
-        
+
         for l in range (len(cash_deficit_amt)):
-            f.write("[{0}}] DAY: {1}, AMOUNT: USD{2}\n".format(cash_details,cash_deficit_day[l],cash_deficit_amt[l]))
+            f.write("[{0}] DAY: {1}, AMOUNT: USD{2}\n".format(cash_details,cash_deficit_day[l],cash_deficit_amt[l]))
     
     else:
         f.write("[{0}] CASH ON EACH DAY IS {1} THAN THE PREVIOUS DAY\n".format(cash_details,cash_results))
