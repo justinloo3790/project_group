271b009678ffe5f6b870894528932e5c76e985f2
username <justinloo85@gmail.com>
2023-01-25 17:29:42 +0800

    hi

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open("summary_report.txt", "w") as f:
     if cash_results == 0:
 
         for l in range (len(cash_deficit_amt)):
-            f.write("[{0}] DAY: {1}, AMOUNT: USD{2}\n".format(cash_details,cash_deficit_day[l],cash_deficit_amt[l]))
+            f.write("[{0}] DAY: {1}, AMOUNT: USD {2}\n".format(cash_details,cash_deficit_day[l],cash_deficit_amt[l]))
     
     else:
         f.write("[{0}] CASH ON EACH DAY IS {1} THAN THE PREVIOUS DAY\n".format(cash_details,cash_results))
@@ -18,7 +18,7 @@ with open("summary_report.txt", "w") as f:
     if profit_results == 0:
 
         for m in range (len(profit_deficit_amt)):
-            f.write("[{0}}] DAY: {1}, AMOUNT: USD{2}\n".format(profit_details,profit_deficit_day[m],profit_deficit_amt[m]))
+            f.write("[{0}}] DAY: {1}, AMOUNT: USD {2}\n".format(profit_details,profit_deficit_day[m],profit_deficit_amt[m]))
     
     else:
         f.write("[{0}] NET PROFIT ON EACH DAY IS {1} THAN THE PREVIOUS DAY\n".format(profit_details,profit_results))
